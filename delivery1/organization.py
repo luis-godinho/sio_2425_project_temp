73575092bcd15158c29168be24c9040edba4cd9d
luisgodinho <luis.godinho13@ua.pt>
2024-11-09 14:40:45 +0000

    dq

--- delivery1/organization.py
+++ delivery1/organization.py
@@ -1,3 +1,7 @@
 class Organization:
     def __init__(self, manager):
         self.manager = manager
+        self.list_documents = []
+
+    def add_to_list(self,s):
+        self.list_documents.append(s)
