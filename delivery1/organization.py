bd45fc3127a07bd9d2487de9a85c0c02ddd3e91f
luisgodinho <luis.godinho13@ua.pt>
2024-11-09 14:40:07 +0000

    some changes

---
+++ delivery1/organization.py
@@ -0,0 +1,3 @@
+class Organization:
+    def __init__(self, manager):
+        self.manager = manager
---
+++ delivery1/subject.py
@@ -0,0 +1,6 @@
+class Subject:
+    def __init__(self, username, full_name, email, public_key):
+        self.username = username
+        self.full_name = full_name
+        self.email = email
+        self.public_key = public_key
