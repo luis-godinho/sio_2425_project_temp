5bf85ec0d8d987d9b69edfd46f31f6e897fb818b
luisgodinho <luis.godinho13@ua.pt>
2024-11-09 15:45:19 +0000

    buiefbiwefbiewfbuiewfbuiwefbiwebuifew

--- delivery1/classes.py
+++ delivery1/classes.py
@@ -12,6 +12,10 @@ class Subject(NamedTuple):
     username: str
     full_name: str
     email: str
+    role_new: bool
+    role_down: bool
+    role_up: bool
+    role_mod: bool
     public_key: str
 
 class Document(NamedTuple):
@@ -67,3 +71,39 @@ def create_org(org_name, manager_id):
     connection.close()
 
     return organization_id
+
+def create_subject(username, full_name, email, pub_key_path):
+    pub_key = ""
+
+    with open(pub_key_path, "w") as f:
+        pub_key = f.read()
+
+    connection = sqlite3.connect("documents")
+    cursor = connection.cursor()
+
+    
+    cursor.execute(
+        """
+        INSERT INTO subject (
+            username, 
+            full_name, 
+            email, 
+            role_new, 
+            role_down, 
+            role_up, 
+            role_mod, 
+            public_key
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+        (username, full_name, email, role_new, role_down, role_up, role_mod, public_key)
+    )
+
+    # Retrieve the subject's ID
+    subject_id = cursor.lastrowid
+
+    # Commit and close
+    connection.commit()
+    connection.close()
+
+    return subject_id
+
--- delivery1/repository.py
+++ delivery1/repository.py
@@ -1,5 +1,5 @@
-from delivery1.classes import create_org, list_all_orgs
-from flask import Flask
+from delivery1.classes import create_org, list_all_orgs, create_subject
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -10,6 +10,18 @@ organizations = {}
 def org_list():
     return json.dumps(list_all_orgs())
 
-@app.route("/organization/create/<org_name>", method=["POST"])
-def create_organization(org_name):
-    create_org(org_name, 0)
+@app.route("/organization/create", method=["POST"])
+def create_organization():
+    data = request.json
+
+    org_name = data.get("organization")
+    username = data.get("username")
+    full_name = data.get("name")
+    email = data.get("email")
+    public_key = data.get("public_key")
+
+    id = create_subject(username, full_name, email, public_key)
+
+    create_org(org_name, id)
+
+
